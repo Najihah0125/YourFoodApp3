cc48c36bacbdf9ed51257e5e4d10f8152afad498
Nur Najihah <nurnajihah.mrizal@petronas.com>
2023-04-09 12:16:57 +0800

    Add project files.

---
+++ YourFoodApp3/YourFoodApp3/Database.cs
@@ -0,0 +1,34 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourFoodApp3
+{
+    public class Database
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<AddedRecipe>();
+        }
+
+        //get all added recipes
+        public Task<List<AddedRecipe>> GetAddedRecipeAsync()
+        {
+            return _database.Table<AddedRecipe>().ToListAsync();
+        }
+        
+        //add recipe
+        public Task<int> AddRecipeAsync(AddedRecipe recipe)
+        {
+            return _database.InsertAsync(recipe);
+        }
+    }
+    
+
+
+}
