23971877f0e988e475528afda1f7e32678192774
Nur Najihah <nurnajihah.mrizal@petronas.com>
2023-04-19 16:11:21 +0800

    Display only bookmarked recipe

--- YourFoodApp3/YourFoodApp3/Bookmark.xaml.cs
+++ YourFoodApp3/YourFoodApp3/Bookmark.xaml.cs
@@ -16,5 +16,21 @@ namespace YourFoodApp3
         {
             InitializeComponent();
         }
+
+        protected override async void OnAppearing()
+        {
+            var bookmark = App.Database.GetBookmarkedRecipeAsync().Result;
+            if (bookmark == null || bookmark.Count == 0)
+            {
+                BookmarkLabel.Text = "Bookmark Empty";
+                collectionView.IsVisible = false;
+            }
+            else
+            {
+                BookmarkLabel.IsVisible = false;
+            }
+            base.OnAppearing();
+            collectionView.ItemsSource = await App.Database.GetBookmarkedRecipeAsync();
+        }
     }
 }
