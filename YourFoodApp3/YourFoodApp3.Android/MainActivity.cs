3e826dc5d4a00a5eb72726a91f4c146726798240
Nur Najihah <nurnajihah.mrizal@petronas.com>
2023-04-25 12:25:18 +0800

    Make Splash Screen

--- YourFoodApp3/YourFoodApp3.Android/MainActivity.cs
+++ YourFoodApp3/YourFoodApp3.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace YourFoodApp3.Droid
 {
-    [Activity(Label = "YourFoodApp3", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "YourFoodApp3", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ YourFoodApp3/YourFoodApp3.Android/SplashActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YourFoodApp3.Droid
+{
+    [Activity(Label = "SplashActivity", Theme = "@style/Splash", MainLauncher = true)]
+    public class SplashActivity : Activity
+    {
+        public SplashActivity()
+        {
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+        }
+    }
+
+}
